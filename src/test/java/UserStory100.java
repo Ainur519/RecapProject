6471a357781fb14c7854229b7ef8d1920318b74b
Ainur <runya579@gmail.com>
2021-06-30 19:47:44 -0400

    User story 100

--- src/test/java/UserStory100.java
+++ src/test/java/UserStory100.java
@@ -1,5 +1,7 @@
 public class UserStory100 {
     public static void main(String[] args) {
-
+        System.out.println("group20");
+        System.out.println("123");
+        System.out.println("Great");
     }
 }
