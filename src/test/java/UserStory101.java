3ba11dd549e6278e29e3ee0605ffccc57b5be567
Ainur <runya579@gmail.com>
2021-06-30 20:22:53 -0400

    user story 101 work

--- src/test/java/UserStory101.java
+++ src/test/java/UserStory101.java
@@ -3,5 +3,6 @@ public class UserStory101 {
         System.out.println("Working on US 101 added some code");
         System.out.println("Added more coding");
         System.out.println("Still working on some code");
+        System.out.println("About to finish work");
     }
 }
