76ccff7dede640dcab80d4916552607178d883b4
Ainur <runya579@gmail.com>
2021-06-30 21:17:10 -0400

    user story 102 third commit

--- src/test/java/UserStory102.java
+++ src/test/java/UserStory102.java
@@ -1,6 +1,7 @@
 public class UserStory102 {
     public static void main(String[] args) {
         System.out.println("Let's work hard");
+        System.out.println("Achieve our goal");
 
     }
 }
